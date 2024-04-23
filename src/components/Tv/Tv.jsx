142770e2d61923e577e43f3129232734aa201dc5
RomanChuykov <rchuykov79@gmail.com>
2024-04-23 21:44:18 +0300

    почав робити зберігання у файл

--- src/components/Tv/Tv.jsx
+++ src/components/Tv/Tv.jsx
@@ -6,17 +6,25 @@ import './Tv.css';
 import { registerLocale, setDefaultLocale } from 'react-datepicker';
 import en from 'date-fns/locale/uk'; // Импортируем нужную локаль
 
+const express=require('express');
+const fs=require('fs');
+const file=express();
+const port=5000;
+
+file.use(express.json())
+
+
 function Tv() {
-  const [date, setDate] = useState(new Date());
+  const [date, setDate] = useState({today:new Date(),endDate:new Date()});
 
   const onChange = (newDate) => {
-    setDate(newDate);
+    setDate({...date,endDate:newDate});
   };
 
   return (
     <div>
       <h1>Tv</h1>
-      <CalendarS onChange={onChange} value={[date,new Date(2024, 4, 15)]} locale="en"/>
+      <CalendarS onChange={onChange} value={[date.today,date.endDate]} locale="en"/>
     </div>
   );
 }
